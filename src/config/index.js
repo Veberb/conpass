551313cfe609046858775d3056bd406a7d683f76
Lucas <lucasveberdebrida@gmail.com>
2019-07-17 10:53:33 -0300

    First blood

---
+++ src/config/index.js
@@ -0,0 +1,27 @@
+const path = require('path');
+const _ = require('lodash');
+
+const env = process.env.NODE_ENV;
+const dev = env === 'development';
+const staging = env === 'staging';
+const production = env === 'production';
+
+const DEFAULT_ENV_CONFIG = {
+	// database
+	db: process.env.MONGO_PATH || 'mongodb://localhost:27017/conpass',
+
+	// app serving on
+	host: process.env.HOST || '127.0.0.1',
+	port: process.env.PORT || 3000,
+
+	// env
+	env,
+	dev,
+	staging,
+	production,
+
+	// app path
+	path: path.normalize(`${__dirname}/..`)
+};
+
+module.exports = _.merge({}, DEFAULT_ENV_CONFIG);
---
+++ src/index.js
@@ -0,0 +1,73 @@
+const express = require('express');
+const Boom = require('boom');
+const morgan = require('morgan');
+const glob = require('glob');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const { MongoMemoryServer } = require('mongodb-memory-server');
+const config = require('./config');
+const app = express();
+
+async function start() {
+	// Enable cors in dev mode
+	if (config.dev) {
+		app.use(cors());
+	}
+
+	// Enable api usage logging
+	if (config.dev) {
+		app.use(morgan('dev'));
+	} else {
+		app.use(morgan('short'));
+	}
+
+	// Enable parsers with larger payloads
+	app.use(express.json({ limit: '25mb', type: 'application/json' }));
+
+	// Connect to MongoDB
+	if (process.env.NODE_ENV === 'test') {
+		const mongoServer = new MongoMemoryServer();
+		mongoServer.getConnectionString().then(mongoUri => {
+			mongoose.connect(mongoUri, {
+				config: { autoIndex: true },
+				useNewUrlParser: true
+			});
+		});
+	} else {
+		mongoose.connect(config.db, {
+			config: { autoIndex: true },
+			useNewUrlParser: true
+		});
+	}
+
+	const db = mongoose.connection;
+	db.on('error', () => {
+		throw new Error(`Unable to connect to database at ${config.db}`);
+	});
+
+	// Load Mongoose Models and Global Helpers
+	const models = glob.sync(`${config.path}/**/*Model.js`);
+	models.forEach(modelPath => {
+		require(`${modelPath}`);
+	});
+
+	// Load APIs
+	const apis = glob.sync(`${config.path}/**/*Api.js`);
+	apis.forEach(apiPath => {
+		require(`${apiPath}`)(app);
+	});
+	// Error handling
+	//eslint-disable-next-line
+	app.use((err, req, res, next) => {
+		const error = Boom.isBoom(err) ? err : Boom.boomify(err);
+		res
+			.status(error.output.statusCode)
+			.json({ message: error.message, error: error.output.payload.error });
+	});
+
+	// Listen the server
+	app.listen(config.port, config.host);
+}
+
+start();
+module.exports = app;
