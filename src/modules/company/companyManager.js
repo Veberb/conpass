93ecd257ba68aa358dfde2d5d0192505378a0736
Lucas <lucasveberdebrida@gmail.com>
2019-07-17 23:08:38 -0300

    Update company

--- src/modules/company/companyManager.js
+++ src/modules/company/companyManager.js
@@ -1,5 +1,6 @@
 const Boom = require('boom');
 const CompanyModel = require('./companyModel');
+const ObjectID = require('mongodb').ObjectID;
 const subscriptionManager = require('../subscription/subscriptionManager');
 
 exports.create = async ({ name, description, owner, planId }) => {
@@ -14,4 +15,18 @@ exports.create = async ({ name, description, owner, planId }) => {
 	subscriptionManager.create({ planId, company: company._id });
 
 	return company;
+};
+
+exports.update = async ({ id, name, description, owner }) => {
+	const $set = {};
+
+	if (name) $set.name = name;
+	if (description) $set.description = description;
+	if (owner) {
+		if (!ObjectID.isValid(owner))
+			throw Boom.badData('Ownder não é um objectId');
+		$set.owner = owner;
+	}
+
+	return CompanyModel.findByIdAndUpdate(id, { $set }, { new: true });
 };
