d38b4ad625bd664561a48f2364a17d4469ffa024
William Godoy <william.s.godoy53@gmail.com>
2019-10-03 21:35:41 -0300

    Defensive first

--- src/modules/flow/flowManager.js
+++ src/modules/flow/flowManager.js
@@ -3,64 +3,62 @@ const FlowModel = require('./flowModel');
 const ObjectID = require('mongodb').ObjectID;
 
 exports.create = async ({ name, description, company, startStep }) => {
-	if (!company || !ObjectID.isValid(company))
-		throw Boom.badRequest('É necessário informar id válido da empresa');
+  if (!company || !ObjectID.isValid(company))
+    throw Boom.badRequest('É necessário informar id válido da empresa');
 
-	if (startStep && !ObjectID.isValid(startStep))
-		throw Boom.badRequest('É necessário informar id válido da primeiro passo?');
+  if (startStep && !ObjectID.isValid(startStep))
+    throw Boom.badRequest('É necessário informar id válido da primeiro passo?');
 
-	const flow = new FlowModel({ name, description, company, startStep });
-	return flow.save();
+  const flow = new FlowModel({ name, description, company, startStep });
+  return flow.save();
 };
 
 exports.update = async ({ id, name, description, company, startStep }) => {
-	const $set = {};
+  if (!ObjectID.isValid(startStep))
+    throw Boom.badData('StartStep não é um objectId válido');
+  if (!ObjectID.isValid(company))
+    throw Boom.badData('Company não é um objectId válido');
 
-	if (name) $set.name = name;
-	if (description) $set.description = description;
-	if (startStep) {
-		if (!ObjectID.isValid(startStep))
-			throw Boom.badData('StartStep não é um objectId válido');
-		$set.startStep = startStep;
-	}
+  const $set = {};
 
-	if (company) {
-		if (!ObjectID.isValid(company))
-			throw Boom.badData('Company não é um objectId válido');
-		$set.company = company;
-	}
+  if (name) $set.name = name;
+  if (description) $set.description = description;
+  if (startStep) {
+    $set.startStep = startStep;
+  }
+  if (company) {
+    $set.company = company;
+  }
 
-	return FlowModel.findByIdAndUpdate(id, { $set }, { new: true });
+  return FlowModel.findByIdAndUpdate(id, { $set }, { new: true });
 };
 
 exports.get = async ({ id }) => {
-	const flow = await FlowModel.findById(id);
-	if (!flow) throw Boom.notFound('Flow não encontrado');
+  const flow = await FlowModel.findById(id);
+  if (!flow) throw Boom.notFound('Flow não encontrado');
 
-	return flow;
+  return flow;
 };
 
-exports.delete = async ({ id }) => {
-	return FlowModel.findByIdAndDelete(id);
-};
+exports.delete = async ({ id }) => FlowModel.findByIdAndDelete(id);
 
 exports.list = async ({
-	name,
-	company,
-	page = 1,
-	limit = 10,
-	order = '-createdAt'
+  name,
+  company,
+  page = 1,
+  limit = 10,
+  order = '-createdAt'
 }) => {
-	const query = {};
+  const query = {};
 
-	if (name) query.name = { $regex: name, $options: 'i' };
-	if (company) query.company = company;
-	const [items, total] = await Promise.all([
-		FlowModel.find(query)
-			.limit(limit)
-			.skip((page - 1) * limit)
-			.sort(order),
-		FlowModel.countDocuments(query)
-	]);
-	return { total, items };
+  if (name) query.name = { $regex: name, $options: 'i' };
+  if (company) query.company = company;
+  const [items, total] = await Promise.all([
+    FlowModel.find(query)
+      .limit(limit)
+      .skip((page - 1) * limit)
+      .sort(order),
+    FlowModel.countDocuments(query)
+  ]);
+  return { total, items };
 };
